59a9683114f30ee79a805aaca910058600f22c7e
csding <csding18@qq.com>
2025-08-12 15:01:58 +0800

    add ascii func in doris

--- sqlglot/dialects/doris.py
+++ sqlglot/dialects/doris.py
@@ -118,6 +118,7 @@ class Doris(MySQL):
             **MySQL.Generator.TRANSFORMS,
             exp.BASE64ENCODE: rename_func("TO_BASE64"),
             exp.BTRIM: rename_func("TRIM_IN"),
+            exp.Unicode: rename_func("ASCII"),
             exp.FromTimestamp: rename_func("DATE_FORMAT"),
             exp.LastValue: _last_value,
             exp.AddMonths: rename_func("MONTHS_ADD"),
